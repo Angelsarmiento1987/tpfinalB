57aecb71960384b0a4e5a42f60ac4111db312a74
unknown <angel_sarm@hotmail.com>
2023-02-02 18:09:47 -0300

    2docommit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+
+    
+
+ 
+
+ 
+
+
   return (
-    <></>
+    <>
+    </>
   );
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,25 +6,40 @@ import { Hero } from '../Hero/Hero';
 
 const Navbar = () => {
 
-    const [valor, setValor] = useState('');
-    const [mostrarResultados, setmostrarResultados] = useState([])
-
+    const [valor, setValor] = useState(''); //creo la variable VALOR que recibira lo que se escriba en el imput
+    const [mostrarResultados, setmostrarResultados] = useState([]) //creo el array mostrarResultados donde se guardaran los datos obtenidps de la api
+    const [mostrarResultados2, setmostrarResultados2] = useState([])
 
     const buscarResultado = async () => {
 
         const apikey = 'ZGkF2bxJelum8E2h6-UzcLmpaa-rp6GX7cwmwiPF9u8' //cargo apikey 
-        const URL = `https://api.unsplash.com/search/photos/?client_id=${apikey}&query=${valor}&per_page=100` //cargo url de la api
+        const URL = `https://api.unsplash.com/search/photos/?client_id=${apikey}&query=${valor}&per_page=20` //cargo url de la api
+        
         
     
         const respuesta = await fetch(URL); //guardo en respuesta la llamada a la api
         const data = await respuesta.json(); //guardo en data la conversion a json 
         setmostrarResultados(data.results) //guardo en mostrarResultados la data
         console.log(data)
+
+       
+     }
+
+     const buscarInfoFoto = async (idFoto) => { 
+
+        const URL2 = `https://api.unsplash.com/photos/${idFoto}/?client_id=ZGkF2bxJelum8E2h6-UzcLmpaa-rp6GX7cwmwiPF9u8`
+
+        const respuesta2 = await fetch(URL2); //guardo en respuesta la llamada a la api
+        const data2 = await respuesta2.json(); //guardo en data la conversion a json 
+        setmostrarResultados2(data2.results) //guardo en mostrarResultados la data
+        console.log(data2.exif.model)
        
 
-        
 
      }
+
+
+
 
 
 
@@ -46,13 +61,28 @@ const Navbar = () => {
                 <div className='seccionFotos'>
                         <div className='contImagenes'>
                         {
-                        mostrarResultados.map((elemento, indice) =>{
+                        mostrarResultados.map((elemento, indice) =>{//deberia ir aca la funcion q cargue por id la api
+
+                              
+
+
+
+
                             return(
+
+
+
+
+
 
                                  <div className='contenedorImagen'>
                                      <img className='imgTamaño' src= {elemento.urls.regular} alt="" />
+                                    <button onClick={()=>buscarInfoFoto(elemento.id)}>Mas info</button> {/*boton q llama la funcion buscarinfofoto y le paso como parametro el identificador de la foto*/}
+
                                      <p>{elemento.user.location}</p>
                                     <p>{elemento.id}</p>
+                                    
+                                   
                             
                               
                                      <p>{elemento.tags[0].title}</p> 
@@ -61,27 +91,27 @@ const Navbar = () => {
                              
 
                                 </div>
-
+                                )  })  }
                       
-
-                                 )
-
-
-                                                                     })
-
-
+     
+                      </div>
+                      </div>
+                      </div>
+                        )                                            
                         }
+                        
+
+                        export { Navbar }
+
 
                  
 
-            </div>
+           
 
 
 
-                </div>
+              
 
-        </div>
-    )
-}
+       
+   
 
-export { Navbar }
